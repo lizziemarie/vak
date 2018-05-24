6c47796d475f166ab3f20879b1283ba23da15d34
lizzie marie <elizabethmariewillett@gmail.com>
2018-05-24 16:27:12 -0700

    xchild first

---
+++ framework/views/integrity/template-layout-portfolio.php
@@ -0,0 +1,25 @@
+<?php
+
+// =============================================================================
+// VIEWS/INTEGRITY/TEMPLATE-LAYOUT-PORTFOLIO.PHP
+// -----------------------------------------------------------------------------
+// Portfolio page output for Integrity. X Child Theme
+// =============================================================================
+
+?>
+
+<?php get_header(); ?>
+
+  <div class="x-container max width offset">
+    <div class="<?php x_main_content_class(); ?>" role="main">
+
+      <?php x_get_view( 'global', '_portfolio' ); ?>
+      <?php x_get_view( 'global', '_content', 'the-content' ); ?>
+
+    </div>
+
+    <?php get_sidebar(); ?>
+
+  </div>
+
+<?php get_footer(); ?>
